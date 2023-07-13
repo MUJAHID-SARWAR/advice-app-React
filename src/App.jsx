cfe0c4281eb023f462feab8fad0440d7ac826998
Mujahid Sarwar <mujahidsarwar288@gmail.com>
2023-07-13 07:40:49 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './App.css'
+import axios from 'axios';
+
+
+class App extends React.Component{
+   state = {advice: ''};
+
+   componentDidMount(){
+    this.fetchAdvice();
+  }
+   fetchAdvice = () => {
+     axios.get('https://api.adviceslip.com/advice')
+     .then((response) =>{
+
+      const {advice} = response.data.slip
+
+      this.setState ({advice});
+
+     })
+.catch ((error) =>{
+  console.log(error)
+
+})
+
+ }
+
+render () {
+ const {advice} = this.state
+  return (
+    
+     <div className="app">
+      <div className="card">
+        <h1 className='heading'>{advice}</h1>
+        <button onClick={this.fetchAdvice} className='button'>
+          <span>GIVE ME ADVICE</span>
+        </button>
+      </div>
+     </div>
+    
+  )
+  }
+  }
+export default App;
